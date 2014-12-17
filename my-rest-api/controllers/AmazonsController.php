c92c5f8090addab7b7a5545a2d4f8200b2d0f0b0
shubham <shubham.agarwal@kelltontech.com>
2014-12-17 12:32:32 +0530

    change in amazon controller

--- my-rest-api/controllers/AmazonsController.php
+++ my-rest-api/controllers/AmazonsController.php
@@ -86,9 +86,17 @@ class AmazonsController
             $image_name = $_GET['key'];
             $user = Users::findById($id);
             $user->profile_image = $image_name;
-            $user->save();
-            $result['profile_image'] = FORM_ACTION.$image_name;
-            Library::output(true, '1', USER_PROFILE_IMAGE, $result);
+            if ($user->save() == false) {
+                foreach ($user->getMessages() as $message) {
+                    $errors[] = $message->getMessage();
+                }
+                Library::logging('error',"API : getStatus amazon controller : ".$errors." : user_id : ".$id);
+                Library::output(false, '0', $errors, null);
+            } else {
+                $result['profile_image'] = FORM_ACTION.$image_name;
+                Library::output(true, '1', USER_PROFILE_IMAGE, $result);
+            }
+            
             
         } catch(Exception $e) {
             Library::logging('error',"API : getStatus : ".$e." ".":user_id : ".$id);
