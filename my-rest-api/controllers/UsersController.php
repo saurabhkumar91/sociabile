ae6cc1fae917f708f3aa9301710c724950fd1416
saurabh kumar <saurabh.kumar@kelltontech.com>
2015-03-10 10:55:18 +0530

    getposts

--- my-rest-api/controllers/UsersController.php
+++ my-rest-api/controllers/UsersController.php
@@ -779,5 +779,46 @@ class UsersController
             Library::output(false, '0', ERROR_REQUEST, null);
         }
      }
+     
+    
+    /**
+     * Method for set display name
+     *
+     * @param object request params
+     * @param object reponse object
+     *
+     * @author Saurabh kumar
+     * @return json
+     */
+    
+     public function setProfileImageAction($header_data,$post_data)
+     { 
+        if( !isset($post_data['username']) || !isset($post_data['birthday']) || !isset($post_data['gender']) || !isset($post_data['hobbies']) || !isset($post_data['about_me'])) {
+            Library::logging('alert',"API : setProfile : ".ERROR_INPUT.": user_id : ".$header_data['id']);
+            Library::output(false, '0', ERROR_INPUT, null);
+        } else {
+            try {
+                $user = Users::findById($header_data['id']);
+                $user->username = $post_data['username'];
+                $user->birthday = $post_data['birthday'];
+                $user->gender = $post_data['gender'];
+                $user->hobbies = $post_data['hobbies'];
+                $user->about_me = $post_data['about_me'];
+                if ($user->save() == false) {
+                    foreach ($user->getMessages() as $message) {
+                        $errors[] = $message->getMessage();
+                    }
+                    Library::logging('error',"API : setProfile : ".$errors." : user_id : ".$header_data['id']);
+                    Library::output(false, '0', $errors, null);
+                } else {
+                    Library::output(true, '1', USER_PROFILE, null);
+                }
+            } catch (Exception $e) {
+                Library::logging('error',"API : setProfile : ".$e." ".": user_id : ".$header_data['id']);
+                Library::output(false, '0', ERROR_REQUEST, null);
+            }  
+        }
+     }
+     
   
 }
