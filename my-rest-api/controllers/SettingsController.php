5199f5f21fe6e036b327246780d350261b09bf6f
saurabh kumar <saurabh.kumar@kelltontech.com>
2015-07-27 15:27:44 +0530

    contactUS format

--- my-rest-api/controllers/SettingsController.php
+++ my-rest-api/controllers/SettingsController.php
@@ -673,7 +673,10 @@ class SettingsController
             //$mail->addReplyTo('replyto@example.com', 'First Last');
             $mail->addAddress(CONTACT_US_EMAIL, '');
             $mail->Subject = 'Contact Us';
-            $message    = "<BR><BR>From : ".$post_data['email_id']."<BR><BR>Date/Time: : ".date('d-m-Y H:i:s')."<BR><BR>Category : ".$category."<BR><BR>Message : ".$post_data['message']."<BR><BR>Device : ".$post_data['user_device']."<BR><BR>Device Model :".$post_data['device_model']."<BR><BR>Version : 1.0";
+            $message    = "<BR><BR>From : ".$post_data['email_id']."<BR><BR>Date/Time: : ".date('d-m-Y H:i:s')."<BR><BR>Category : ".$category."<BR><BR>Message : ".$post_data['message']."<BR><BR>Device : ".$post_data['user_device']."<BR><BR>Device Model :".$post_data['device_model']."<BR><BR>App Version : ".$header_data["version"]."<BR><BR>User ID : ".$header_data["id"];
+            if( isset($post_data['os_version']) ){
+                $message    .= "<BR><BR>Device OS-Version : ".$post_data['os_version'];
+            }
             $mail->msgHTML($message, dirname(__FILE__));
             $mail->AltBody = $post_data['message'];
             if( !empty( $_FILES["image"]['tmp_name'] ) ){
